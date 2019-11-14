89bf60f8752f65f5f87e16d1b73beab303afb822
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-14 11:24:56 -0500

    Fix syntax for sub-modules, add commas

--- fiveBitAdder.v
+++ fiveBitAdder.v
@@ -9,46 +9,46 @@ wire carryOut, carryOut2, carryOutt3, carryOut4;
 
 FULLADDER add1
 (
-    .a({A[0]}) 
-    .b({B[0]})
-    .ci({1'b1})
-    .co(carryOut)
+    .A({A[0]}), 
+    .B({B[0]}),
+    .Ci({1'b1}),
+    .Co(carryOut),
     .SUM(SUM[0])
 );
 
 FULLADDER add2
 (
-    .a({A[1]}) 
-    .b(B[1])
-    .ci({carryOut})
-    .co(carryOut2)
+    .A({A[1]}), 
+    .B(B[1]),
+    .Ci({carryOut}),
+    .Co(carryOut2),
     .SUM(SUM[1])
 );
 
 FULLADDER add3
 (
-    .a({A[2]}) 
-    .b({B[2]})
-    .ci({carryOut2})
-    .co(carryOut3)
+    .A({A[2]}), 
+    .B({B[2]}),
+    .Ci({carryOut2}),
+    .Co(carryOut3),
     .SUM(SUM[2])
 );
 
 FULLADDER add4
 (
-    .a({A[0]}) 
-    .b({B[0]})
-    .ci({carryOut3})
-    .co(carryOut4)
+    .A({A[0]}), 
+    .B({B[0]}),
+    .Ci({carryOut3}),
+    .Co(carryOut4),
     .SUM(SUM[3])
 );
 
 FULLADDER add5
 (
-    .a({A[0]}) 
-    .b({B[0]})
-    .ci({carryOut4})
-    .co()
+    .A({A[0]}), 
+    .B({B[0]}),
+    .Ci({carryOut4}),
+    .Co(),
     .SUM(SUM[4])
 );
 
