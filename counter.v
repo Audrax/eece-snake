af7b0a483ff6242066c37bbfc90010d6c6466df3
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-14 11:55:47 -0500

    Add flip-flop with enable

--- counter.v
+++ counter.v
@@ -7,40 +7,83 @@ module counter
 
 wire [4:0] muxout;
 wire [4:0] addout;
+wire [4:0] dffwire;
 
 mux #(2,1) mux1
 (
     .data_out(muxout[0]),
     .select_in({opcode}),
-    .data_in({addout[0],1'b0})
+    .data_in({dffwire[0],1'b0})
 );
 
 mux #(2,1) mux2
 (
     .data_out(muxout[1]),
     .select_in({opcode}),
-    .data_in({addout[1],1'b0})
+    .data_in({dffwire[1],1'b0})
 );
 
 mux #(2,1) mux3
 (
     .data_out(muxout[2]),
     .select_in({opcode}),
-    .data_in({addout[2],1'b0})
+    .data_in({dffwire[2],1'b0})
 );
 
 mux #(2,1) mux4
 (
     .data_out(muxout[3]),
     .select_in({opcode}),
-    .data_in({addout[3],1'b0})
+    .data_in({dffwire[3],1'b0})
 );
 
 mux #(2,1) mux5
 (
     .data_out(muxout[4]),
     .select_in({opcode}),
-    .data_in({addout[4],1'b0})
+    .data_in({dffwire[4],1'b0})
+);
+
+
+
+dff_en dff1
+(
+    .data_out(dffwire[0]),
+    .in_D(addout[0]),
+    .in_CLK(clock),
+    .in_EN(en)
+);
+
+dff_en dff2
+(
+    .data_out(dffwire[1]),
+    .in_D(addout[1]),
+    .in_CLK(clock),
+    .in_EN(en)
+);
+
+dff_en dff3
+(
+    .data_out(dffwire[2]),
+    .in_D(addout[2]),
+    .in_CLK(clock),
+    .in_EN(en)
+);
+
+dff_en dff4
+(
+    .data_out(dffwire[3]),
+    .in_D(addout[3]),
+    .in_CLK(clock),
+    .in_EN(en)
+);
+
+dff_en dff5
+(
+    .data_out(dffwire[4]),
+    .in_D(addout[4]),
+    .in_CLK(clock),
+    .in_EN(en)
 );
 
 fiveBitAdder add1
