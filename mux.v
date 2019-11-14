f900bfff67b5559e9a7d3aca0be47b32e43fb2d2
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-14 11:49:45 -0500

    Add Flip-Flop and Mux files

---
+++ mux.v
@@ -0,0 +1,15 @@
+module mux
+    #(parameter NUM_INPUTS = 32, parameter NUM_OUTPUTS = 1)
+    (
+    input [(NUM_INPUTS-1):0] data_in,
+    input [$clog2(NUM_INPUTS)-1:0]  select_in,
+    output data_out
+    );
+reg [NUM_INPUTS:0] D;
+always @ (D)
+    begin
+        D <= data_in;
+    end
+assign data_out = D[select_in];
+
+endmodule
