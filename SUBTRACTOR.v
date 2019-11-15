73b805ab5cc62bdb3ce95754dcff0c1621b462b5
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-15 15:14:33 -0500

    Add subtractor

---
+++ SUBTRACTOR.v
@@ -0,0 +1,51 @@
+module fiveBitSubtractor
+(
+    input [4:0] A,
+    input [4:0] B,
+    output [4:0] D
+);
+
+wire carryOut1, carryOut2, carryOut3, carryout4;
+wire [4:0] B_Not;
+
+not not1(B_Not, B);
+
+FULLADDER add1
+(
+    .A(A[0]),
+    .B(B[0]),
+    .Co(carryOut1),
+    .Ci(0'b1)
+)
+
+FULLADDER add2
+(
+    .A(A[1]),
+    .B(B[1]),
+    .Co(carryOut2),
+    .Ci(0'b1)
+)
+
+FULLADDER add3
+(
+    .A(A[2]),
+    .B(B[2]),
+    .Co(carryOut3),
+    .Ci(0'b1)
+)
+
+FULLADDER add4
+(
+    .A(A[3]),
+    .B(B[3]),
+    .Co(carryOut4),
+    .Ci(0'b1)
+)
+
+FULLADDER add5
+(
+    .A(A[4]),
+    .B(B[4]),
+    .Co(),
+    .Ci(0'b1)
+)
