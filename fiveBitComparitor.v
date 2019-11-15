e9ae99069b79974e3e1248ae3064b0323cd88eec
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-15 14:20:44 -0500

    Separate comparitor from counter

---
+++ fiveBitComparitor.v
@@ -0,0 +1,15 @@
+module fiveBitComparitor
+(
+    input [4:0] A,
+    input [4:0] B,
+    output F
+);
+
+wire [4:0]xnorwire;
+
+xnor xnor1(xnorwire[0], A[0], B[0]);
+xnor xnor2(xnorwire[1], A[1], B[1]);
+xnor xnor3(xnorwire[2], A[2], B[2]);
+xnor xnor4(xnorwire[3], A[3], B[3]);
+xnor xnor5(xnorwire[4], A[4], B[4]);
+nand and1(F, xnorwire[0], xnorwire[1], xnorwire[2], xnorwire[3], xnorwire[4]);
