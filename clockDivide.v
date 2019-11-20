99fded1765db5b2b362ed2cbfb093361f3c32660
Audrax <28691369+Audrax@users.noreply.github.com>
2019-11-20 16:33:10 -0500

    Fix clock divider to use flip-flops

--- clockDivide.v
+++ clockDivide.v
@@ -1,25 +1,32 @@
 module clockDivide
 (
     input clock100,
-    output clock25
+    output clock25,
 );
 
-wire dffwire, subwire;
+wire dffwire1, dffwire2;
+wire notwire1, notwire2;
+
+not not1(notwire1, dffwire1);
 
 dff_en dff1
 (
-    .D(subwire),
-    .clk(clock100),
-    .data_out(dffwire)
+    .in_D(notwire1),
+    .in_CLK(clock100),
+    .in_EN(1'b1),
+    .data_out(dffwire1)
 );
 
-assign clock25 = dffwire;
+not not2(notwire2, dffwire2);
 
-tenBitSubtractor sub1
+dff_en dff2
 (
-    .A({clock100}),
-    .B({0'b0,0'b0,0'b0,0'b0,0'b0,0'b0,0'b0,0'b0,1'b1,0'b0}),
-    .D({subwire})
+    .in_D(notwire2),
+    .in_CLK(dffwire1),
+    .in_EN(1'b1),
+    .data_out(dffwire2)
 );
+
+assign clock25 = dffwire2;
 
 endmodule
