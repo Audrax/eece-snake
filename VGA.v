178a1e5a254efb1816a2ffc08a08784aaf3bfc22
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-21 10:41:55 -0500

    Update VGA module

--- VGA.v
+++ VGA.v
@@ -1,41 +1,49 @@
-module VGA(HS, VS, R, G, B, clk)
-{
+module VGA
+(
     output HS,
     output VS,
     output R,
     output G,
     output B,
     input clk
-}
+);
+
+wire clk25, hCountWire, vCountWire
+
+clockDivide divide
+(
+    .clock100(clk),
+    .clock25(clk25)
+);
 
 tenBitCounter hCount
 (
-    .max(),
-    .en_debounce(),
-    .clock(),
-    .count()
+    .max(10'b0111100000),
+    .en_debounce(clk25),
+    .clock(clk25),
+    .count(hCountWire)
 );
 
 synchro hSync
 (
-    .D(),
-    .clock(),
-    .Q()
+    .D(hCountWire),
+    .clock(clk25),
+    .Q(HS)
 );
 
 tenBitCounter vCount
 (
-    .max(),
-    .en_debounce(),
-    .clock(),
-    .count()
+    .max(10'b1010000000),
+    .en_debounce(clk25),
+    .clock(clk25),
+    .count(vCountWire)
 );
 
 synchro vSync
 (
-    .D(),
-    .clock(),
-    .Q()
+    .D(vCountWire),
+    .clock(clk25),
+    .Q(VS)
 );
 
 endmodule
