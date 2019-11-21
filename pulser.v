5b6908545f4e3983d35373b977b0be1468da2ca4
Matthew <28691369+Audrax@users.noreply.github.com>
2019-11-21 12:27:00 -0500

    Add pulser

---
+++ pulser.v
@@ -0,0 +1,22 @@
+module pulser
+(
+    input D,
+    input clock
+    output Q
+);
+
+wire qWire, dWire;
+
+nor nor1(dWire, D, qWire);
+
+dff_en dff1
+(
+    in_D(dWire),
+    in_CLK(clock),
+    in_en(1'b1),
+    data_out(qWire)
+);
+
+buf buf1(Q, qWire);
+
+endmodule
