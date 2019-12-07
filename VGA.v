744ff83dbe91f3d7e2ab80fbea5ff39f1e793594
Audrax <28691369+Audrax@users.noreply.github.com>
2019-12-07 15:29:29 -0500

    Remove temp debug code

--- VGA.v
+++ VGA.v
@@ -2,16 +2,18 @@ module VGA
 (
     output HS,
     output VS,
-    output [3:0] R,
+    output hCount,
+    output vCount,
+    /*output [3:0] R,
     output [3:0] G,
     output [3:0] B,
-    input tempSwR, tempSwG, tempSwB,
+    input tempSwR, tempSwG, tempSwB,*/
     input clk
 );
 
 // Temp Switches to drive RGB Pins
 
-and BUFR0(R[0], tempSwR, hSyncWire, vSyncWire);
+/*and BUFR0(R[0], tempSwR, hSyncWire, vSyncWire);
 and BUFR1(R[1], tempSwR, hSyncWire, vSyncWire);
 and BUFR2(R[2], tempSwR, hSyncWire, vSyncWire);
 and BUFR3(R[3], tempSwR, hSyncWire, vSyncWire);
@@ -24,7 +26,7 @@ and BUFG3(G[3], tempSwG, hSyncWire, vSyncWire);
 and BUFB0(B[0], tempSwB, hSyncWire, vSyncWire);
 and BUFB1(B[1], tempSwB, hSyncWire, vSyncWire);
 and BUFB2(B[2], tempSwB, hSyncWire, vSyncWire);
-and BUFB3(B[3], tempSwB, hSyncWire, vSyncWire);
+and BUFB3(B[3], tempSwB, hSyncWire, vSyncWire); */
 
 wire [19:0] hCountWire, vCountWire;
 wire clk25, hSyncWire, vSyncWire, hSetWire, hResetWire, vSetWire, vResetWire;
@@ -39,7 +41,7 @@ clockDivide divide
 
 // Horizontal Counter + Sync
 
-twentyBitCounter hCount
+twentyBitCounter hCounter
 (
     .max(20'b00000000001100100000), //800
     .en(1'b1),
@@ -71,7 +73,7 @@ sr_latch hLatch
 
 // Vertical Counter + Sync
 
-twentyBitCounter vCount
+twentyBitCounter vCounter
 (
     .max(20'b01100101110000100000), //416,800
     .en(hSyncWire),
@@ -101,8 +103,13 @@ sr_latch vLatch
     .Q_Not()
 );
 
+// Prop wires to sync pins
 buf bufHS(HS, hSyncWire);
 buf bufVS(VS, vSyncWire);
+
+// Output count addresses
+buf bufHCount(hCount, hCountWire);
+buf bufVCount(vCount, vCountWire);
 
 endmodule
 
